321db5cf07ecf448f7dd1557eef67254eb619c7d
Kamisorara <1210281722@qq.com>
2023-05-08 19:54:07 +0800

    first version

---
+++ init.lua
@@ -0,0 +1,6 @@
+require('config.lazy') 		-- 运行基础配置
+require('config.autocmds')       -- 自动运行
+require('config.keymaps')       -- 快捷键
+require('config.options')       -- 基础配置
+require('config.neovide')              -- neovide gui配置
+require('config.run') -- 程序运行
---
+++ lua/config/lazy.lua
@@ -0,0 +1,92 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require('lazy').setup({
+    spec = { 
+        {import = "plugins"} 
+    },
+    defaults = {
+        lazy = false,
+        version = false,
+    },
+    lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json", -- lockfile generated after running update.
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    ui = {
+        -- a number <1 is a percentage., >1 is a fixed size
+        size = { width = 0.8, height = 0.8 },
+        -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+        border = "single",
+        icons = {
+        cmd = " ",
+        config = "",
+        event = "",
+        ft = " ",
+        init = " ",
+        import = " ",
+        keys = " ",
+        lazy = "鈴 ",
+        loaded = "●",
+        not_loaded = "○",
+        plugin = " ",
+        runtime = " ",
+        source = " ",
+        start = "",
+        task = "✔ ",
+        list = {
+            "●",
+            "➜",
+            "★",
+            "‒",
+        },
+    },
+    -- leave nil, to automatically select a browser depending on your OS.
+    -- If you want to use a specific browser, you can define it here
+    browser = nil, ---@type string?
+    throttle = 20, -- how frequently should the ui process render events
+    -- you can define custom key maps here.
+    -- To disable one of the defaults, set it to false
+    custom_keys = {},
+  },   
+    performance = {
+    cache = {
+      enabled = true,
+      path = vim.fn.stdpath("cache") .. "/lazy/cache",
+      -- Once one of the following events triggers, caching will be disabled.
+      -- To cache all modules, set this to `{}`, but that is not recommended.
+      -- The default is to disable on:
+      --  * VimEnter: not useful to cache anything else beyond startup
+      --  * BufReadPre: this will be triggered early when opening a file from the command line directly
+      disable_events = { "UIEnter", "BufReadPre" },
+      ttl = 3600 * 24 * 5, -- keep unused modules for up to 5 days
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
+      ---@type string[]
+      paths = {}, -- add any custom paths here that you want to indluce in the rtp
+      ---@type string[] list any plugins you want to disable here
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },   
+})
+
