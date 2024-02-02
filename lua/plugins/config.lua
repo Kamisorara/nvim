9ca0e9e5c05d1581b68a25fcffdadf5819257c7d
Kamisorara <rakamiso575@gmail.com>
2024-02-02 12:25:29 +0800

    add lint

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -1342,6 +1342,25 @@ config.lspsaga = {
     end,
 }
 
+-- lint配置
+config["nvim-lint"] = {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+        require("lint").linters_by_ft = {
+            javascript = { "eslint" },
+            typescript = { "eslint" },
+            javascriptreact = { "eslint" },
+            typescriptreact = { "eslint" },
+        }
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            callback = function()
+                require("lint").try_lint()
+            end,
+        })
+    end,
+}
+
 Kamisora.plugins = config
 Kamisora.keymap.prefix = {
     ["<leader>b"] = { name = "+buffer" },
